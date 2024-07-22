5a03632351bc83aca3d68a365b01208f56d92de0
Patrik Zahálka <zahalka.patrik@gmail.com>
2024-07-22 13:04:09 +0200

    refactor: Change outputs dir path to project root directory

--- packages/global_sim/essentials_tb.vhd
+++ packages/global_sim/essentials_tb.vhd
@@ -4,8 +4,8 @@ package essentials_tb is
 
     constant PROJ_NAME : string := "fpga-tools-qc";
     constant PROJ_DIR : string := "C:/Git/zahapat/fpga-tools-qc/";
-    constant RANDOM_SEED_1 : natural := 1021485030;
-    constant RANDOM_SEED_2 : natural := 1752907755;
+    constant RANDOM_SEED_1 : natural := 211182145;
+    constant RANDOM_SEED_2 : natural := 475879320;
 
 end package essentials_tb;
 
